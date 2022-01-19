ab6fe14f88b10249d64fa4ef4418fb48954d5ffd
Mohammad <mohammadmahone@gmail.com>
2022-01-20 00:51:02 +0330

    Adding cause and season to DeathListItem component.

--- src/Components/DeathListItem.js
+++ src/Components/DeathListItem.js
@@ -4,6 +4,8 @@ const DeathListItem = ({ item }) =>
         <div className="card-death">
             <h2><span className="span-header">Death:</span> {item.death}</h2>
             <span>Responsible: <p>{item.responsible}</p></span>
+            <span>Cause: <p>{item.cause}</p></span>
+            <span>Season: <p>{item.season}</p></span>
             <span>Episode: <p>{item.episode}</p></span>
             <span>Number of deaths: <p>{item.number_of_deaths}</p></span>
             <span>Last Words: <p>{item.last_words}</p></span>
