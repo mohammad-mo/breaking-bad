8f3619e4400088f09550503b9a7a2c87868e4b5a
Mohammad <mohammadmahone@gmail.com>
2022-01-25 14:00:47 +0330

    Adding new animation to page transition.

--- src/animation.js
+++ src/animation.js
@@ -2,17 +2,22 @@ export const pageAnimation =
 {
     hidden: {
         opacity: 0,
+        x: 100
     },
     show: {
         opacity: 1,
+        x: 0,
         transition: {
-            duration: 0.5
+            duration: 0.4,
+            ease: 'easeIn'
         }
     },
     exit: {
         opacity: 0,
+        x: -100,
         transition: {
-            duration: 0.5
+            duration: 0.6,
+            ease: 'easeOut'
         }
     }
 }
