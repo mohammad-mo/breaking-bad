a168d80bec1c2199a1ce4433c1fb2b3ac8c3da2f
Mohammad <mohammadmahone@gmail.com>
2022-01-25 14:34:42 +0330

    Update animation.js

--- src/animation.js
+++ src/animation.js
@@ -2,22 +2,17 @@ export const pageAnimation =
 {
     hidden: {
         opacity: 0,
-        x: 100
     },
     show: {
         opacity: 1,
-        x: 0,
         transition: {
-            duration: 0.4,
-            ease: 'easeIn'
+            duration: 0.5
         }
     },
     exit: {
         opacity: 0,
-        x: -100,
         transition: {
-            duration: 0.6,
-            ease: 'easeOut'
+            duration: 0.5
         }
     }
 }
