adb3d6a523d474efd93866ac4339549ce8a1209b
Mohammad <mohammadmahone@gmail.com>
2022-05-03 05:28:24 +0430

    - New animation.

--- src/animation.js
+++ src/animation.js
@@ -1,9 +1,11 @@
 export const pageAnimation = {
   hidden: {
     opacity: 0,
+    y: 150,
   },
   show: {
     opacity: 1,
+    y: 0,
     transition: {
       duration: 0.5,
       when: 'beforeChildren',
@@ -12,6 +14,7 @@ export const pageAnimation = {
   },
   exit: {
     opacity: 0,
+    y: 150,
     transition: {
       duration: 0.5,
     },
